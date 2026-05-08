41b600d95e7228cc7b35dbdb6205d6b4f69cfeb2
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2026-05-08 06:17:43 -0300

    enable blink

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,22 +1,9 @@
--- Filename: ~/.config/neovim/neobean/lua/plugins/blink-cmp.lua
--- ~/.config/neovim/neobean/lua/plugins/blink-cmp.lua
-
--- HACK: blink.cmp updates | Remove LuaSnip | Emoji and Dictionary Sources | Fix Jump Autosave Issue
--- https://youtu.be/JrgfpWap_Pg
-
--- completion plugin with support for LSPs and external sources that updates
--- on every keystroke with minimal overhead
-
--- https://www.lazyvim.org/extras/coding/blink
--- https://github.com/saghen/blink.cmp
--- Documentation site: https://cmp.saghen.dev/
-
 -- NOTE: Specify the trigger character(s) used for luasnip
 local trigger_text = ";"
 
 return {
 	"saghen/blink.cmp",
-	enabled = false,
+	enabled = true,
 	-- In case there are breaking changes and you want to go back to the last
 	-- working release
 	-- https://github.com/Saghen/blink.cmp/releases
@@ -24,10 +11,8 @@ return {
 	dependencies = {
 		"moyiz/blink-emoji.nvim",
 		"Kaiser-Yang/blink-cmp-dictionary",
-		"giuxtaposition/blink-cmp-copilot",
 		"tpope/vim-dadbod",
 		"kristijanhusak/vim-dadbod-completion",
-		"zbirenbaum/copilot.lua",
 		"L3MON4D3/LuaSnip",
 	},
 	opts = function(_, opts)
